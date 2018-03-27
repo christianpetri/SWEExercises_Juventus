8fb2e6beef2356cc9d6fda9318c139f0b6364332
christianpetri <christianpetri@hotmail.com>
2018-03-27 21:29:28 +0200

    Logger installiert

---
+++ ch.juventu.java.test/test/MainTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author : ${user}
+ * @since: ${date}
+ */
+public class MainTest {
+
+    @Test
+    public void main() {
+    }
+}
