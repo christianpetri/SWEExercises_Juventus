54ba67a251eaa64d9c5632fa1bcdace8d72123a3
christianpetri <christianpetri@hotmail.com>
2018-04-04 00:13:52 +0200

    20180404 Unterricht Übungen Array

--- ch.juventu.java.test/test/MainTest.java
+++ ch.juventu.java.test/test/MainTest.java
@@ -2,6 +2,8 @@ package test;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -12,5 +14,16 @@ public class MainTest {
 
     @Test
     public void main() {
+        /*
+        MathUtils calc = new MathUtils();
+        double result = calc.subtraction(1,15);
+        System.out.println( "calc.subtraction(1,15) --> " + result );
+        assertEquals(14,result);
+        List<Double> numbers = Arrays.asList(1.38, 2.56, 4.3);
+        result = calc.addition(numbers);
+        System.out.println("calc.addition(1.38, 2.56, 4.3) --> " + result );
+        result = calc.subtraction(5,3);
+        System.out.println("calc.subtraction(5,3) --> "  + result );
+        */
     }
 }
