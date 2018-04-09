264344d91e8f989b2dff8ceb6f64c445da93a0ac
christianpetri <christianpetri@hotmail.com>
2018-04-09 08:43:06 +0200

    Javadoc ("re-upload") was not yet included in the vcs

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"ch.juventus.java.javadoc"},{"l":"object"}]
---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Main"},{"p":"ch.juventus.java.javadoc","l":"MathUtils"},{"p":"object","l":"Person"}]
